36c9a23f99f67f30835d9028047a52f5d845be76
刘文壮 <liuwenzhuang@corp.netease.com>
2021-01-07 11:23:48 +0800

    fix: 选择图片文件不在工作区内某文件夹的根路径下，插入图片标签文件名重复的问题

--- markdown-insert-images/src/commands/insert-img.ts
+++ markdown-insert-images/src/commands/insert-img.ts
@@ -57,10 +57,11 @@ async function openImgPicker(textEditor: vscode.TextEditor, workspace: vscode.Wo
 
   const firstFile = selectedFileUris[0];
   const firstFilePath = firstFile.fsPath;
+  const firstFileDir = path.dirname(firstFilePath);
   /**
    * 当前文件路径相对于workspace根路径的相对路径，用于插入的文本
    */
-  let relativePath = path.relative(rootPath, firstFilePath);
+  let relativePath = path.relative(rootPath, firstFileDir);
 
   const beyondWorkspace = firstFilePath.indexOf(rootPath) === -1;
   let copyResult:
@@ -70,7 +71,7 @@ async function openImgPicker(textEditor: vscode.TextEditor, workspace: vscode.Wo
       }
     | undefined = undefined;
   if (beyondWorkspace) {
-    copyResult = await copyFilesToWorkspace(workspace.uri, selectedFileUris);
+    copyResult = await copyFilesToWorkspace(workspace, selectedFileUris);
   }
   if (copyResult) {
     if (!copyResult.isCopied) {
@@ -83,7 +84,7 @@ async function openImgPicker(textEditor: vscode.TextEditor, workspace: vscode.Wo
   state.defaultUri[workspaceName] = vscode.Uri.file(path.dirname(firstFilePath));
 
   // 如果选择的是根目录下的文件，relativePath就是文件名
-  if (relativePath.indexOf(path.sep) === -1) {
+  if (relativePath === '') {
     relativePath = '/';
   } else {
     relativePath = '/' + relativePath.replace(/\\/g, '/'); // 插入文档中需转换为unix路径
@@ -94,27 +95,52 @@ async function openImgPicker(textEditor: vscode.TextEditor, workspace: vscode.Wo
 
 /**
  * 将workspace外的文件复制到当前workspace下的某文件夹
- * @param workspaceUri
+ * @param workspace
  * @param selectedFileUris
  */
-async function copyFilesToWorkspace(workspaceUri: vscode.Uri, selectedFileUris: vscode.Uri[]) {
+async function copyFilesToWorkspace(
+  workspace: vscode.WorkspaceFolder,
+  selectedFileUris: vscode.Uri[]
+) {
+  const workspaceUri = workspace.uri;
   const rootPath = workspaceUri.fsPath;
+  const workspaceName = workspace.name;
+  if (!state.defaultSaveUri || !state.defaultSaveUri[workspaceName]) {
+    state.defaultSaveUri[workspaceName] = workspaceUri;
+  }
+
+  const defaultSaveUri = state.defaultSaveUri[workspaceName];
+
+  const selectBtn = '选择文件夹' as const;
+  const selectPrevBtn = '使用上次选中' as const;
+  const cancelBtn = '取消' as const;
   const result = await vscode.window.showWarningMessage(
-    '文件不在当前工程下，复制到当前工程下的文件夹？',
-    'Confirm',
-    'Cancel'
+    `文件不在当前工程下，可选择当前工程内的文件夹进行复制，或直接使用上次选中的文件夹（${defaultSaveUri.fsPath}）。`,
+    selectBtn,
+    selectPrevBtn,
+    cancelBtn
   );
-  if (result !== 'Confirm') {
+  if (!result || result === cancelBtn) {
     return;
   }
-  const copyDestUris = await vscode.window.showOpenDialog({
-    title: '所选文件将复制到此文件夹下',
-    defaultUri: workspaceUri,
-    canSelectMany: false,
-    canSelectFiles: false,
-    canSelectFolders: true,
-    openLabel: '复制',
-  });
+
+  let copyDestUris: vscode.Uri[] | undefined = [];
+  if (result === selectBtn) {
+    copyDestUris = await vscode.window.showOpenDialog({
+      title: '所选文件将复制到此文件夹下',
+      defaultUri: state.defaultSaveUri[workspaceName],
+      canSelectMany: false,
+      canSelectFiles: false,
+      canSelectFolders: true,
+      openLabel: '复制',
+    });
+  }
+
+  if (result === selectPrevBtn) {
+    // 使用上一次的文件夹
+    copyDestUris = [defaultSaveUri];
+  }
+
   if (!copyDestUris) {
     return;
   }
@@ -124,14 +150,18 @@ async function copyFilesToWorkspace(workspaceUri: vscode.Uri, selectedFileUris: 
     vscode.window.showErrorMessage('请选择当前工程下的文件夹');
     return;
   }
+
+  // 保存选中的目的文件夹
+  state.defaultSaveUri[workspaceName] = copyDestUri;
+
   let isCopied = true;
   let relativePath = '';
   selectedFileUris.forEach((fileUri) => {
     const filePath = fileUri.fsPath;
     const fileName = path.basename(filePath);
     try {
-      const destFilePath = path.join(copyDestPath, fileName);
-      relativePath = path.relative(rootPath, destFilePath); // 重新计算相对路径
+      relativePath = path.relative(rootPath, copyDestPath); // 重新计算相对路径
+      const destFilePath = path.join(copyDestPath, fileName); // 文件的目的路径
       fs.copyFileSync(filePath, destFilePath);
     } catch (err) {
       vscode.window.showErrorMessage(err.message);
--- markdown-insert-images/src/store.ts
+++ markdown-insert-images/src/store.ts
@@ -5,9 +5,17 @@ type DefaultUri = {
 };
 
 const state: {
+  /**
+   * 工作区内各文件夹对应的上一次选择的文件夹
+   */
   defaultUri: DefaultUri;
+  /**
+   * 工作区内各文件夹对应的上一次选择的外部文件保存的目的文件夹
+   */
+  defaultSaveUri: DefaultUri;
 } = {
   defaultUri: {},
+  defaultSaveUri: {}
 };
 
 export default state;
